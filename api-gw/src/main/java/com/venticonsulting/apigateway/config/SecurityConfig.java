df1496145d6b4e6c61a68ce350cf1a1bf3bb5891
Amati Angelo <amati.angelo@20m2official.it>
2023-10-25 17:10:57 +0200

    add auth

--- api-gw/src/main/java/com/venticonsulting/apigateway/config/SecurityConfig.java
+++ api-gw/src/main/java/com/venticonsulting/apigateway/config/SecurityConfig.java
@@ -6,23 +6,23 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
-//@Configuration
-//@EnableWebFluxSecurity
+@Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
-//  @Bean
-//  public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity){
-//    // all request made to retrieve static resources (like the one for the eureka html and css static resources) are allowed without authentication
-//    serverHttpSecurity
-//      .csrf()
-//      .disable()
-//      .authorizeExchange(
-//        exchange -> exchange.pathMatchers("/eureka/**").permitAll().anyExchange().authenticated()
-//      ).oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
-//
-//    return serverHttpSecurity.build();
-//
-//  }
+  @Bean
+  public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity){
+    // all request made to retrieve static resources (like the one for the eureka html and css static resources) are allowed without authentication
+    serverHttpSecurity
+      .csrf()
+      .disable()
+      .authorizeExchange(
+        exchange -> exchange.pathMatchers("/eureka/**").permitAll().anyExchange().authenticated()
+      ).oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+
+    return serverHttpSecurity.build();
+
+  }
 
 
 }
