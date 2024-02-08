02d854328c3442502f138ee25d4d4b4446b797f5
Amati Angelo <amati.angelo@20m2official.it>
2024-02-08 10:16:40 +0100

    try to fix discovery problems

--- api-gw/src/main/java/com/ventimetriconsulting/configuration/CorsConfig.java
+++ api-gw/src/main/java/com/ventimetriconsulting/configuration/CorsConfig.java
@@ -1,13 +1,14 @@
 package com.ventimetriconsulting.configuration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.util.pattern.PathPatternParser;
 
 @Configuration
+@Slf4j
 public class CorsConfig {
 
     @Bean
@@ -20,16 +21,5 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfig);
         return new CorsWebFilter(source);
-
-//        CorsConfiguration config = new CorsConfiguration();
-//        config.setAllowCredentials(true);
-//        config.addAllowedOrigin("*"); // Or use config.addAllowedOrigin("*"); for allowing all origins
-//        config.addAllowedHeader("*");
-//        config.addAllowedMethod("*");
-//
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
-//        source.registerCorsConfiguration("/**", config);
-
-        //return new CorsWebFilter(source);
     }
 }
---
+++ api-gw/src/main/java/com/ventimetriconsulting/utils/JwtUtil.java
@@ -0,0 +1,21 @@
+package com.ventimetriconsulting.utils;
+
+import org.springframework.stereotype.Component;
+import java.security.Key;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.Jwts;
+@Component
+public class JwtUtil {
+
+    public static final String SECRET = "4df568215f062018329c3edaca57bb83a7007e18d42943b980ac48ff0e995982";
+
+    public void validateToken(final String token) {
+        Jwts.parser().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
